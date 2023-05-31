1bdf7c49ba4f1eb2f7a1abb8e1119edb2267133a
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-05-31 16:11:12 -0300

    Update RegisterPage.js

--- 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/RegisterPage/RegisterPage.js
+++ 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/RegisterPage/RegisterPage.js
@@ -1,1 +1,30 @@
-oi
+import { View } from "react-native";
+import styles from "./styles";
+import { useState } from "react"
+import { TextInput } from "react-native"
+
+export default function RegisterPage(){
+
+    const [nome, setNome] = useState("")
+    const [usuario, setUsuario] = useState("")
+    const [senha, setSenha] = useState("")
+
+    return (
+        <View style = {styles.container}>
+           <TextInput style = {styles.input}
+                value = {nome}
+                onChangeText={text=>setNome(text)}
+                placeholder="Nome completo"/>
+            <TextInput style = {styles.input}
+                value = {usuario}
+                onChangeText={text=>setUsuario(text)}
+                placeholder="e-mail"/>
+            <TextInput style = {styles.input}
+                value = {senha}
+                onChangeText={text=>setSenha(text)}
+                placeholder="senha"
+                secureTextEntry = 'true'/>
+        </View>
+    )
+}
+
