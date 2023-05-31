6129d6c73ad2fba0b01c03d564f43f4a2789c6c3
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-05-31 16:47:03 -0300

    Create styles.js

---
+++ 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/RegisterPage/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#0D0D0D",
+        alignItems: 'center'
+    },
+
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 20,
+        borderColor: "#088C7F",
+        color: "#fff",
+        width: 250
+      }
+});
+
+export default styles;
