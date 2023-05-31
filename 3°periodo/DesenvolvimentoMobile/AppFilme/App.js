ee2eaebdd11c0f3ab3c0c5e2f7b2977d1291a3d0
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-05-31 16:47:47 -0300

    Update App.js

--- 3°periodo/DesenvolvimentoMobile/AppFilme/App.js
+++ 3°periodo/DesenvolvimentoMobile/AppFilme/App.js
@@ -1,1 +1,19 @@
-oi
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import RegisterPage from './src/Pages/RegisterPage/RegisterPage';
+import LoginPage from './src/Pages/LoginPage/LoginPage';
+import FilmePage from './src/Pages/FilmePage/FilmePage';
+
+const Stack =  createStackNavigator();
+
+export default function App() {
+    return(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "Página de login" component={LoginPage}/>
+          <Stack.Screen name = "Página de registro" component={RegisterPage}/>
+          <Stack.Screen name = "Página de filmes" component={FilmePage}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+}
