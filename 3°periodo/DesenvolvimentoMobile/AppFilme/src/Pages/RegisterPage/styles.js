d9b264fd84cb0ffd10d717b749616ab7673e2cab
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-06-12 19:45:03 -0300

    Update styles.js

--- 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/RegisterPage/styles.js
+++ 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/RegisterPage/styles.js
@@ -16,7 +16,26 @@ const styles = StyleSheet.create({
         borderColor: "#088C7F",
         color: "#fff",
         width: 250
-      }
+      },
+
+      Btn: {
+        backgroundColor: "#088C7F",
+        color: '#F2CB05',
+        width: 160,
+        height: 40,
+        borderRadius: 20,
+        alignItems: 'center',
+        marginBottom: 10,
+        /*paddingVertical: 12,
+        paddingHorizontal: 32,*/
+    },
+
+    txtBtn: {
+        color: '#F2CB05',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
 });
 
 export default styles;
