96160cd9ebf73efb64d3f2c971f1044189e0620a
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-06-12 19:43:11 -0300

    Update Filme.js

--- 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/FilmePage/Filme.js
+++ 3°periodo/DesenvolvimentoMobile/AppFilme/src/Pages/FilmePage/Filme.js
@@ -1,4 +1,5 @@
 import { Image, Text, View, TouchableOpacity } from "react-native";
+import { Link } from '@react-navigation/native';
 import styles from "./styles";
 
 const baseURL = "https://api.otaviolube.com";
@@ -9,10 +10,14 @@ export default function Filme({filme}){
         <View style = {styles.containerFilme}>
             <Image source = {{uri: baseURL + filme.poster.data.attributes.formats.small.url}} style = {styles.imagem}/>
             <Text style = {styles.tituloFilme}>{filme.titulo} <br/>{filme.subtitulo}</Text> 
-            <Text style = {styles.sinopse}>{filme.sinopse}</Text>
+            <Text style = {styles.sinopse}>{filme.sinopse.length > 200 ? filme.sinopse.substring(0, 200) + "..." : filme.sinopse }</Text>
             <TouchableOpacity style = {styles.Btn}>
-                <Text style = {styles.txtBtn}>Visualizar</Text>
+                <Link to= {{screen: "Página de detalhes", params: {filme: filme}}} style = {styles.txtBtn}>Visualizar</Link>
             </TouchableOpacity>       
         </View>
     );
 }
+
+
+
+
