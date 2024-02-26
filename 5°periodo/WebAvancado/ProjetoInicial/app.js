043c4c29df4b32791755293471c6084e3405524b
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2024-02-26 08:44:23 -0300

    upload

--- 5°periodo/WebAvancado/ProjetoInicial/app.js
+++ 5°periodo/WebAvancado/ProjetoInicial/app.js
@@ -1,24 +1,67 @@
 import fastify from 'fastify';
+import createError from '@fastify/error';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const MyCustomError = createError('MyCUstomError', 'Something stranged happened.', 501);
 
 export async function build(opts){
     const app = fastify(opts);
 
-    app.get('/', async (request, reply) =>{
-        return{hello: 'world'}
+    app.register(fastifyStatic, {
+        root: path.join(__dirname, 'public'),
+        wildcard: false
+    });
+
+    app.get('/*', async (request, reply) => {
+        request.log.info({params: request.params}, 'Hello from wildcard');
+        return reply.sendFile('index.html');
     });
 
     const products = [
-        {id: 1, name: 'Tomate', qntd: 20},
-        {id: 2, name: 'Cebola', qntd: 50}
+        {id: 1, name: 'Tomate', qtd: 20},
+        {id: 2, name: 'Cebola', qtd: 50}
     ]
 
-    app.get('/products', async (request, reply) =>{
-        return products
+    app.get('/products', async (request, reply) => {
+        return products;
     });
 
-    app.get('/products/:id', async (request, reply) =>{
-        app.log.info('Produto requisitado-.>' + request.params.id);
+    app.post('/products', async (request, reply) => {
+        let product = request.body;
+        return {product};
+    });
+
+    app.get('/products/:id', async (request, reply) => {
+        app.log.info('Produto requisitado> ' + request.params.id);
         return {};
+    });
+    
+    app.delete('/products/:id', async (request, reply) => {
+        app.log.info('Produto para remover> ' + request.params.id);
+        return {};
+    });
+
+    app.get('/error', (req, rep) => {
+        throw new MyCustomError;
+    });
+
+    app.setErrorHandler(async (error, req, rep) => {
+        req.log.error({error});
+        rep.code(error.statusCode || 500);
+
+        return `Route ${request.url} caused an Internal Server Error`; 
+    });
+
+    app.setNotFoundHandler(async (req, rep) => {
+        reply.code(404);
+
+        return 'Resource not found';
     });
 
     return app;
--- 5°periodo/WebAvancado/ProjetoInicial/server.js
+++ 5°periodo/WebAvancado/ProjetoInicial/server.js
@@ -1,7 +1,21 @@
 import { build } from './app.js';
+import dotenv from 'dotenv';
+import closeWithGrace from 'close-with-grace';
+
+dotenv.config();
 
 const opts = {logger: true}
 
 const app = await build(opts);
 
-await app.listen({port: 3000});
+const port = process.env.PORT || '3000';
+const host = process.env.HOST || '127.0.0.1';
+
+await app.listen({port, host});
+
+closeWithGrace(async ({signal, error}) =>{
+    if (error)
+        app.log.error(`Server Close due to an error: ${error.message}`);
+    else
+        app.log.info(`${signal} signal received. Shutting down gracefully`);
+});
